a213ca4a0634d13ed2fffe255545c988772dc01e
Mugisha Elie <iradukunda2k24@gmail.com>
2025-09-29 16:13:23 +0200

    ES6

---
+++ ES6/letconst.js
@@ -0,0 +1,39 @@
+// function checkVar(){
+//     if(true){
+//         const x = 10;
+//         x = 20;
+//     }
+
+//     console.log(x);
+// }
+
+// checkVar();
+
+// function getClothing(isCold){
+//     let freezing = "true";
+//     const static = "yes";
+//     if(isCold){
+//         let freezing = "Grab a Jacket";
+//         const static = "yes"; 
+//     }else{
+//         var hot = "It's a shorts kind of day";
+//         console.log(freezing);
+//     }
+// }
+
+// getClothing(false)
+
+let str = `<ul>
+    <li>Item1</li>
+</ul>`;
+
+console.log(str);
+
+let x = 10;
+let y = 20; 
+
+console.log(x, y);
+
+[x, y] = [y, x];
+
+console.log(x, y);
