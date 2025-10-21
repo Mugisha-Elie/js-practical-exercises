92a9c776e05f35828cae2c4f3a761e9735e4a5db
Mugisha-Elie <iradukunda2k24@gmail.com>
2025-10-21 19:08:30 +0200

    changes: codewars

--- ES6/deadFish.js
+++ ES6/deadFish.js
@@ -2,26 +2,20 @@ function parse( data )
 {
     let value = 0;
     const result = [];
-
-    // const result = data.split("").map(char => {
-    //     if(char === "i"){
-    //         count++;
-    //     }else{
-
-    //     }
-    //     return count
-    // })
     
     data = data.split("");
     for(let i=0; i<data.length; i++){
         if(data[i] === "i"){
             value++
-        }else if(data[i] === "s"){
-            value *= value;
-        }else if(data[i] === "d"){
-            value--;
-        }else if(data[i] === "o"){
+        }
+        if(data[i] === "d"){
+            value--
+        }
+        if(data[i] === "o"){
             result.push(value)
+        }
+        if(data[i] === "s"){
+            value = Math.pow(value, 2)
         }
     }
     return result;
---
+++ codewars/addOne.js
@@ -0,0 +1,14 @@
+function upArray(arr){
+  let result = arr.map((num) => {
+    let numL = num.toString().length;
+    if(numL > 1){
+        return null;
+    }else
+})
+  return result;
+}
+
+console.log(upArray([1,2,3,4]))
+console.log(upArray([1,2,3,4,9]))
+console.log(upArray([12,2,1]))
+console.log(upArray([1,2,-1]))
