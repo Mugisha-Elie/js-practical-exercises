3afab5f6a94127d1a88e94fad96165fe54a149eb
Mugisha-Elie <iradukunda2k24@gmail.com>
2025-11-02 10:34:10 +0200

    es6: variables

---
+++ ES6 Practice/varables.js
@@ -0,0 +1,37 @@
+// var language = "JavaScript"
+// // console.log(globalThis)
+
+// let lang = "Python"
+// // console.log(global)
+
+// for(var i=0; i<5; i++){
+//     setTimeout(function(){
+//         console.log(i);
+//     }, 1000)
+// }
+
+// for(var i=0; i<5; i++){
+//     (function(j){
+//         setTimeout(function(){
+//             console.log(j);
+//         }, 1000)
+//     })(i)
+// }
+
+
+let variable;
+console.log(variable)
+
+let log = function(){
+    console.log(message)
+}
+
+
+
+let message = "Hi"
+
+console.log(log())
+
+for(const k = 0; k<5; k++){
+    console.log(k)
+}
