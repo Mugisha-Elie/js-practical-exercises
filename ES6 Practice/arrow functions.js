01cef3e8eca125a4b5640b858fe990399b90ea6c
Mugisha-Elie <iradukunda2k24@gmail.com>
2025-11-05 08:08:28 +0200

    chore: arrow function vs this

--- ES6 Practice/arrow functions.js
+++ ES6 Practice/arrow functions.js
@@ -13,4 +13,19 @@ let setColor = function(color){
 } 
 
 let backgroundColor = setColor('Red');
-console.log(backgroundColor.value)
+// console.log(backgroundColor.value)
+
+
+function Car(){
+    this.speed = 0;
+    this.speedUp = function(speed){
+        this.speed = speed
+        let self = this;
+        setTimeout(function(){
+            console.log(self.speed)
+        }, 1000)
+    }
+}
+
+let car = new Car();
+car.speedUp(50)
