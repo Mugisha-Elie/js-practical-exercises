8e71f9bc1c101fbd84643a50d9b0d05dfd301ec4
Mugisha-Elie <iradukunda2k24@gmail.com>
2025-11-17 19:30:58 +0200

    codewars: longestConsec

---
+++ codewars/longestConsec.js
@@ -0,0 +1,14 @@
+function longestConsec(strarr, k) {
+    const newArray = strarr.map((elem, index, arr)=>{
+        if(arr.length === 0 || ){
+            elem = elem + arr[index + 1]
+            return elem;
+        }else{
+            return
+        }
+    })
+
+    return newArray
+}
+
+console.log(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3))
