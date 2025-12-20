2902599c39fe89de7c6b3023da08ffee37e7460c
Mugisha-Elie <iradukunda2k24@gmail.com>
2025-12-20 20:13:02 +0200

    Promises

--- JavaScript Promises/multipleResources.js
+++ JavaScript Promises/multipleResources.js
@@ -6,15 +6,15 @@ async function fetchResources(url){
 
 // fulfills when all the promises resolve and reject if one of them is not fulfilled
 
-// Promise.all([
-//     fetchResources("https://gist.githubusercontent.com/Mugisha-Elie/4c176028b4bc43c2d34eecde175bf9f0/raw/74e16b33711d25b0dc066a35f2d01b872e1641a6/gistfile1.json"), 
+Promise.all([
+    fetchResources("https://gist.githubusercontent.com/Mugisha-Elie/4c176028b4bc43c2d34eecde175bf9f0/raw/74e16b33711d25b0dc066a35f2d01b872e1641a6/gistfile1.json"), 
 
-//     fetchResources("https://gist.githubusercontent.com/Mugisha-Elie/aa2168509cfefe24050e14c5346e6891/raw/f5c93849fa7b0666dd28393964ca7a2b66054daa/gistfile2.json"),
+    fetchResources("https://gist.githubusercontent.com/Mugisha-Elie/aa2168509cfefe24050e14c5346e6891/raw/f5c93849fa7b0666dd28393964ca7a2b66054daa/gistfile2.json"),
 
-//     fetchResources("https://gist.githubusercontent.com/Mugisha-Elie/a71e7ec580c5f7e4be6bd703b2b4e0a9/raw/ff08a512c80d29ffb0c1f72e61a11c45b7465fa1/gistfile3.json")
-// ])
-// .then(values => console.log(values))
-// .catch(error => console.log("Error fetching resources"))
+    fetchResources("https://gist.githubusercontent.com/Mugisha-Elie/a71e7ec580c5f7e4be6bd703b2b4e0a9/raw/ff08a512c80d29ffb0c1f72e61a11c45b7465fa1/gistfile3.json")
+])
+.then(values => console.log(values))
+.catch(error => console.log("Error fetching resources"))
 
 
 
@@ -29,15 +29,15 @@ async function fetchResources(url){
 
 // fulfills when all the promises settle
 
-Promise.allSettled([
-    fetchResources("https://gist.githubusercontent.com/Mugisha-Elie/4c176028b4bc43c2d34eecde175bf9f0/raw/74e16b33711d25b0dc066a35f2d01b872e1641a6/gistfile1.json"), 
+// Promise.allSettled([
+//     fetchResources("https://gist.githubusercontent.com/Mugisha-Elie/4c176028b4bc43c2d34eecde175bf9f0/raw/74e16b33711d25b0dc066a35f2d01b872e1641a6/gistfile1.json"), 
 
-    fetchResources("https://gist.githubusercontent.com/Mugisha-Elie/aa2168509cfefe24050e14c5346e6891/raw/f5c93849fa7b0666dd28393964ca7a2b66054daa/gistfile2.json"),
+//     fetchResources("https://gist.githubusercontent.com/Mugisha-Elie/aa2168509cfefe24050e14c5346e6891/raw/f5c93849fa7b0666dd28393964ca7a2b66054daa/gistfile2.json"),
 
-    fetchResources("https://gist.githubusercontent.com/Mugisha-Eli/a71e7ec580c5f7e4be6bd703b2b4e0a9/raw/ff08a512c80d29ffb0c1f72e61a11c45b7465fa1/gistfile3.json")
-])
-.then(values => console.log(values))
-.catch(error => console.log("Error fetching resources"))
+//     fetchResources("https://gist.githubusercontent.com/Mugisha-Eli/a71e7ec580c5f7e4be6bd703b2b4e0a9/raw/ff08a512c80d29ffb0c1f72e61a11c45b7465fa1/gistfile3.json")
+// ])
+// .then(values => console.log(values))
+// .catch(error => console.log("Error fetching resources"))
 
 
 
